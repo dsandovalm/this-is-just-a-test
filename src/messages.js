4f2b4eb3b65f7f804b4c2849adcece5b349db6b6
DianaCato <diana.catolico@hotmail.com>
2021-06-09 10:41:58 -0500

    nuevos mensajes

--- src/messages.js
+++ src/messages.js
@@ -1,19 +1,22 @@
 export default [
     {
-        message:'Prueba 1',
-        author:'autor001'
+        message:'Hola Dani quiero desearte el mayor de los éxitos, de verdad que sé que te va a ir muy bien, eres una persona muy paciente, trabajadora y organizada, esta decisión que tomas no es fácil tomarla, pero siempre he admirado a las personas que persiguen sus sueños, espero que te vaya muy bien!!! Y tengo la certeza que te irá muy bien.',
+        author:'Astrid Piñero'
     },
     {
         message:'Daniel, gracias por compartir tu tiempo y conocimientos durante el tiempo que logramos compartir en este espacio tan enriquecedor para todos. Los mejores deseos en el camino que deseas emprender, recordando que “… la vida es corta y el mar es enorme” ...que seas feliz haciendo lo que amas.',
         author:'Gloria Cuevas'
     },
     {
-        message:'Dani,  que este  nuevo  camino que emprendes este lleno logros, gracias por el  acompañamiento de estos meses, fue muy constructivo conocer  una persona como  tu, me quedo con grandes aprendizajes de ti,  algo  que aplicare en mi camino laboral de aqui en adelante... Los mejores deseos y la mejor energia! ',
-        author:'Tati Roa',
-        image: './images/tati.jpg'
+        message:"Dani,  que este  nuevo  camino que emprendes este lleno logros, gracias por el  acompañamiento de estos meses, fue muy constructivo conocer  una persona como  tu, me quedo con grandes aprendizajes de ti,  algo  que aplicare en mi camino laboral de aqui en adelante... Los mejores deseos y la mejor energia!" ,
+        author:'Tati Roa'
     },
     {
-        message:'Me impulsaste a hacer cosas increibles, que siempre voy a recordar con mucho cariño. Gracias por tu buena energia! ',
+        message:`Que este  nuevo  camino que emprendes este lleno logros <img src="./images/tati.jpg" alt="Gracias Dani" width="350" > `,
+        author:'Tati Roa'
+    },
+    {
+        message:'Me impulsaste a hacer cosas increibles, me ayudaste a mejorar y apreciar mi trabajo. Siempre, siempre voy a recordarte con mucho cariño. Gracias por tu buena energia! ',
         author:'Diana Católico'
     },
 ]
