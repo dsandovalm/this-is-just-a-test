420ec80f7cba79c4e7a84d5debd9f72d6d4445ef
DianaCato <diana.catolico@hotmail.com>
2021-06-08 20:42:29 -0500

    mensaje Tatiana

--- src/messages.js
+++ src/messages.js
@@ -8,8 +8,9 @@ export default [
         author:'autor002'
     },
     {
-        message:'Prueba 3',
-        author:'autor003'
+        message:'Dani,  que este  nuevo  camino que emprendes este lleno logros, gracias por el  acompañamiento de estos meses, fue muy constructivo conocer  una persona como  tu, me quedo con grandes aprendizajes de ti,  algo  que aplicare en mi camino laboral de aqui en adelante... Los mejores deseos y la mejor energia! ',
+        author:'Tati Roa',
+        image: './images/tati.jpg'
     },
     {
         message:'Me impulsaste a hacer cosas increibles, que siempre voy a recordar con mucho cariño. Gracias por tu buena energia! ',
