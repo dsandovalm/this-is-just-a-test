b0649fa79462a6604f1f9f493525010661e3a472
DianaCato <diana.catolico@hotmail.com>
2021-06-09 09:02:45 -0500

    mensaje de Gloria

--- src/messages.js
+++ src/messages.js
@@ -4,8 +4,8 @@ export default [
         author:'autor001'
     },
     {
-        message:'Prueba 2',
-        author:'autor002'
+        message:'Daniel, gracias por compartir tu tiempo y conocimientos durante el tiempo que logramos compartir en este espacio tan enriquecedor para todos. Los mejores deseos en el camino que deseas emprender, recordando que “… la vida es corta y el mar es enorme” ...que seas feliz haciendo lo que amas.',
+        author:'Gloria Cuevas'
     },
     {
         message:'Dani,  que este  nuevo  camino que emprendes este lleno logros, gracias por el  acompañamiento de estos meses, fue muy constructivo conocer  una persona como  tu, me quedo con grandes aprendizajes de ti,  algo  que aplicare en mi camino laboral de aqui en adelante... Los mejores deseos y la mejor energia! ',
